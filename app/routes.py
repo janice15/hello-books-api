6ae5abd59cf0ed17231a8ad1f0e7643cab1b339c
Janice <janicenguyenn@gmail.com>
2023-05-01 17:25:37 -0700

    create route to read all books and create a book

--- app/routes.py
+++ app/routes.py
@@ -16,10 +16,11 @@ from flask import Blueprint, jsonify, abort, make_response, request
 #     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
 # ]
 
+
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
@@ -28,6 +29,20 @@ def handle_books():
     db.session.commit()
 
     return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
 
 #def validate_book(book_id):
 #    try:
