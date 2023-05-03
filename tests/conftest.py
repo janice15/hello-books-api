2f174c4f637f27e2f36b1bc23dd9601fe9108c3f
Janice <janicenguyenn@gmail.com>
2023-05-03 10:09:11 -0700

    created GET /book/<book_id> Test

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,3 +25,15 @@ def client(app):
     return app.test_client()
 
 
+@pytest.fixture
+def two_saved_books(app):
+    # Arrange
+    ocean_book = Book(title="Ocean Book",
+                      description="watr 4evr")
+    mountain_book = Book(title="Mountain Book",
+                         description="i luv 2 climb rocks")
+    db.session.add_all([ocean_book, mountain_book])
+def test_get_one_book(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    # ...
