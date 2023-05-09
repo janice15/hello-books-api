03a0d1792558cfbb19bf75cad67552cb84425ed4
Janice <janicenguyenn@gmail.com>
2023-05-08 18:28:13 -0700

    created nested route to author_id/books

--- app/author_routes.py
+++ app/author_routes.py
@@ -1,18 +1,24 @@
 from app import db
 from app.models.author import Author
 from flask import Blueprint, jsonify, abort, make_response, request
-
+from app.models.book import Book
+from app.routes import validate_model
 authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
 
-@authors_bp.route("", methods=["POST"])
-def create_author():
+@authors_bp.route("/<author_id>/books", methods=["POST"])
+def create_book(author_id):
+
+    author = validate_model(Author, author_id)
+
     request_body = request.get_json()
-    new_author = Author(name=request_body["name"],)
-
-    db.session.add(new_author)
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author=author
+    )
+    db.session.add(new_book)
     db.session.commit()
-
-    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
 
 @authors_bp.route("", methods=["GET"])
 def read_all_authors():
