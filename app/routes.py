890a4c3ed28166ca0d9c453ae1566a8351871018
Janice <janicenguyenn@gmail.com>
2023-05-08 18:09:37 -0700

    refactored validate_book to use class method

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,16 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-def validate_book(book_id):
+def validate_book(cls,book_id):
     try:
         book_id = int(book_id)
     except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+        abort(make_response({"message":f"{cls.__name__} {book_id} invalid"}, 400))
 
-    book = Book.query.get(book_id)
+    book = cls.query.get(book_id)
 
     if not book:
-        abort(make_response({"message":f"book {book_id} not found"}, 404))
+        abort(make_response({"message":f"{cls.__name__} {book_id} not found"}, 404))
 
     return book
 
